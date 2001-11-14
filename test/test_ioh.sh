0a25967a3d1960b3ba050d091d14928277b13be4
Seth Robertson <seth@counterstorm.com>
2001-11-14 19:11:07 +0000

    IOH performance tool

---
+++ test/test_ioh.sh
@@ -0,0 +1,28 @@
+#!/bin/perl
+#
+#
+#
+
+
+
+$receive{'ttcp'} = 'ttcp -p $port -r > /proj/startide/seth/big.out';
+$trans{'ttcp'} = 'ttcp -p $port -t 127.1 < /proj/startide/seth/big.in';
+$receive{'bdttcp'} = 'bdttcp -p $port -r 127.1 < /dev/null > /proj/startide/seth/big.out';
+$trans{'bdttcp'} = 'bdttcp -p $port -t 127.1 < /proj/startide/seth/big.in';
+$receive{'test_ioh'} = 'test_ioh --ioh-inbuf-hint=8192 -p $port -r < /dev/null > /proj/startide/seth/big.out';
+$trans{'test_ioh'} = 'test_ioh --ioh-inbuf-hint=8192 -p $port -t 127.1 < /proj/startide/seth/big.in';
+$receive{'test_ioh-S'} = 'test_ioh --ioh-inbuf-hint=8192 -Sp $port -r < /dev/null > /proj/startide/seth/big.out';
+$trans{'test_ioh-S'} = 'test_ioh --ioh-inbuf-hint=8192 -Sp $port -t 127.1 < /proj/startide/seth/big.in';
+$receive{'test_ioh-Sl'} = 'test_ioh --ioh-inbuf-hint=8192 -Sp $port -r < /dev/null > /proj/startide/seth/big.out';
+$trans{'test_ioh-Sl'} = 'test_ioh --ioh-outbuf-max=20000 --ioh-inbuf-hint=8192 -Sp $port -t 127.1 < /proj/startide/seth/big.in';
+
+$ENV{'port'}=6010;
+
+foreach $cmd ('ttcp','ttcp','ttcp','bdttcp','bdttcp','bdttcp','test_ioh','test_ioh','test_ioh','test_ioh-S','test_ioh-S','test_ioh-S','test_ioh-Sl','test_ioh-Sl','test_ioh-Sl')
+{
+  print "$cmd $receive{$cmd} &\n";
+  system("$receive{$cmd} &");
+  sleep(5);
+  system("time $trans{$cmd}");
+  $ENV{'port'}++;
+}
