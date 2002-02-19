94f7a4ae77c018582d485875d50ae330a3b82484
brian <brian>
2002-02-19 21:45:58 +0000

    brian needs arp header. - "je demande!"

--- include/libbk_include.h
+++ include/libbk_include.h
@@ -1,5 +1,5 @@
 /*
- * $Id: libbk_include.h,v 1.17 2002/02/06 21:36:37 brian Exp $
+ * $Id: libbk_include.h,v 1.18 2002/02/19 21:45:58 brian Exp $
  *
  * ++Copyright LIBBK++
  *
@@ -101,6 +101,7 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
+#include <net/if_arp.h>
 #include <arpa/inet.h>
 #include <netinet/in_systm.h>
 #include <netinet/ip.h>
