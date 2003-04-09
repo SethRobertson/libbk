a1a22c017501ba17091795f68cdc9f3181e27b88
Seth Robertson <seth@counterstorm.com>
2003-04-09 20:11:36 +0000

    Accidentially deleted old contents when creating new routines.

--- lib/b_thread.c
+++ lib/b_thread.c
@@ -1,5 +1,5 @@
 #if !defined(lint) && !defined(__INSIGHT__)
-static const char libbk__rcsid[] = "$Id: b_thread.c,v 1.4 2003/04/09 03:57:14 seth Exp $";
+static const char libbk__rcsid[] = "$Id: b_thread.c,v 1.5 2003/04/09 20:11:36 seth Exp $";
 static const char libbk__copyright[] = "Copyright (c) 2001";
 static const char libbk__contact[] = "<projectbaka@baka.org>";
 #endif /* not lint */
@@ -98,6 +98,130 @@ struct bk_threadcomm
 
 static void *bk_thread_continue(void *opaque);
 
+
+
+
+#ifdef BK_USING_PTHREADS
+
+
+/**
+ * Thread safe counting initialization
+ *
+ * THREADS: MT-SAFE
+ *
+ * @param B Baka thread/global environment
+ * @param bac Atomic counter structure
+ * @param start Starting value
+ * @param flags Fun for the future
+ * @return <i>zero</i> on success
+ * @return <br><i>-1</i> on call failure, lock failure
+ */
+int bk_atomic_add_init(bk_s B, struct bk_atomic_cntr *bac, int start, bk_flags flags)
+{
+  BK_ENTRY(B, __FUNCTION__, __FILE__, "libbk");
+  int ret;
+
+  if (!bac)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Invalid arguments\n");
+    BK_RETURN(B, -1);
+  }
+
+  if ((ret = pthread_mutex_init(&bac->bac_lock, NULL)) != 0)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Could not init mutex (%d): %s\n", ret, strerror(errno));
+    BK_RETURN(B, -1);
+  }
+
+  bac->bac_cntr = start;
+
+  BK_RETURN(B, 0);
+}
+
+
+
+/**
+ * Thread safe counting/addition/subtraction
+ *
+ * THREADS: MT-SAFE
+ *
+ * @param B Baka thread/global environment
+ * @param bac Atomic counter structure
+ * @param delta Number which we add to counter (negative to subtract, zero to probe)
+ * @param result Optional copy-out of result of operation
+ * @param flags Fun for the future
+ * @return <i>zero</i> on success
+ * @return <br><i>-1</i> on call failure, lock failure
+ */
+int bk_atomic_addition(bk_s B, struct bk_atomic_cntr *bac, int delta, int *result, bk_flags flags)
+{
+  BK_ENTRY(B, __FUNCTION__, __FILE__, "libbk");
+  int ret, myresult;
+
+  if (!bac)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Invalid arguments\n");
+    BK_RETURN(B, -1);
+  }
+
+  if ((ret = pthread_mutex_lock(&bac->bac_lock)) != 0)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Could not lock mutex (%d): %s\n", ret, strerror(errno));
+    BK_RETURN(B, -1);
+  }
+
+  myresult = bac->bac_cntr += delta;
+
+  if ((ret = pthread_mutex_unlock(&bac->bac_lock)) != 0)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Could not unlock mutex (%d): %s\n", ret, strerror(errno));
+    BK_RETURN(B, -1);
+  }
+
+  if (result)
+    *result = myresult;
+
+  BK_RETURN(B, 0);
+}
+
+
+
+/**
+ * Blocking-non-blocking method to acquire pthread mutex lock.
+ *
+ * @param B BAKA Thread/global state
+ * @return <i>0</i> on success
+ * @return <i>negative</i> on error
+ */
+int bk_pthread_mutex_lock(bk_s B, struct bk_run *run, pthread_mutex_t *mutex, bk_flags flags)
+{
+  BK_ENTRY(B, __FUNCTION__, __FILE__, "libbk");
+
+  if (!run || !mutex)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Internal error: invalid arguments\n");
+    BK_RETURN(B, -1);
+  }
+
+  while (pthread_mutex_trylock(mutex) != 0)
+  {
+    if (errno == EBUSY)
+    {
+      bk_run_once(B, run, BK_RUN_ONCE_FLAG_DONT_BLOCK);
+    }
+    else
+ {
+      bk_error_printf(B, BK_ERR_ERR, "Mutex lock acquisition failed: %s.\n", strerror(errno));
+      goto error;
+    }
+  }
+
+  BK_RETURN(B, 0);
+
+ error:
+  BK_RETURN(B, -1);
+}
+#endif /* BK_USING_PTHREADS */
 
 
 
