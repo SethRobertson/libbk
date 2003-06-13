36c9b0ae51f6c3fbbe14bc4ccefa96412986afa3
Seth Robertson <seth@counterstorm.com>
2003-06-13 02:55:11 +0000

    Experimentation with gettimeofday  precision

---
+++ test/gettimeofprecision.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMBER 4*1024*1024
+
+main(int argc, char *argv[])
+{
+  struct timeval tv[NUMBER];
+  int x;
+
+  for (x=0; x<NUMBER; x++)
+  {
+    gettimeofday(&tv[x],NULL);
+  }
+
+  for (x=1; x<NUMBER; x++)
+  {
+    struct timeval tvm;
+    tvm.tv_sec = tv[x].tv_sec - tv[x-1].tv_sec;
+    tvm.tv_usec = tv[x].tv_usec - tv[x-1].tv_usec;
+
+    while (tvm.tv_usec < 0)
+    {
+      tvm.tv_sec--;
+      tvm.tv_usec += 1000000;
+    }
+
+    while (tvm.tv_usec > 1000000)
+    {
+      tvm.tv_sec--;
+      tvm.tv_usec -= 1000000;
+    }
+
+    //printf("%d %d.%06d\n", x, tvm.tv_sec, tvm.tv_usec);
+    //printf("%d %d.%06d\n", x, tv[x].tv_sec, tv[x].tv_usec);
+    printf("%d.%06d %d.%06d\n", tv[x].tv_sec, tv[x].tv_usec, tvm.tv_sec, tvm.tv_usec);
+  }
+}
