ec56b85c68eafd79e56bed1d8ea64bd3169c4616
brian <brian>
2004-05-04 14:40:39 +0000

    fun defs in headers? only for inline functions but this one's too big.

--- include/libbk_inline.h
+++ include/libbk_inline.h
@@ -1,5 +1,5 @@
 /*
- * $Id: libbk_inline.h,v 1.15 2003/07/16 18:23:00 brian Exp $
+ * $Id: libbk_inline.h,v 1.16 2004/05/04 14:40:38 brian Exp $
  *
  * ++Copyright LIBBK++
  *
@@ -108,7 +108,8 @@ static __inline__ void 		bk_dll_iterate_done(dict_h handle, dict_iter iter);
 static __inline__ dict_obj 	bk_dll_nextobj(dict_h handle, dict_iter iter);
 static __inline__ char *	bk_dll_error_reason(dict_h handle, int *errnop);
 static __inline__ char *	bk_dll_error_reason(dict_h handle, int *errnop);
-static __inline__ int		bk_dll_insert_internal(struct bk_generic_dll_handle *gdh, struct bk_generic_dll_element *gde, struct bk_generic_dll_element **old_gdep, int flags, int append);
+/* defined in b_dll.c */
+extern int		bk_dll_insert_internal(struct bk_generic_dll_handle *gdh, struct bk_generic_dll_element *gde, struct bk_generic_dll_element **old_gdep, int flags, int append);
 
 
 /**
@@ -232,90 +233,6 @@ do									\
 
 
 
-/**
- * Insert a dll element onto the list. In keeping with CLC insert
- * prepends. Again flags is supporessed to keep this in line with the CLC
- * APi.
- *
- *	@param handle The handle of dll.
- *	@param obj The object to insert.
- *	@return !<i>DICT_ERR</i> on failure.<br>
- *	@return <i>DICT_OK</i> on success.
- */
-static __inline__ int
-bk_dll_insert_internal(struct bk_generic_dll_handle *gdh, struct bk_generic_dll_element *gde, struct bk_generic_dll_element **old_gdep, int flags, int append)
-{
-  struct bk_generic_dll_element *g;
-  int ret = DICT_OK;
-  int compare;
-
-  if (!flags)
-  {
-    // General case where we want an unordered list and duplicates are fine.
-    if (append)
-      BK_DLL_INSERT_AFTER(gdh, gde, gdh->gdh_tail);
-    else
-      BK_DLL_INSERT_BEFORE(gdh, gde, gdh->gdh_head);
-  }
-  else
-  {
-    // This sections covers *both* ordering and unique keys, but at least one must be present.
-    int ordered = flags & DICT_ORDERED;
-    int unique_keys = flags & DICT_UNIQUE_KEYS;
-
-    if (old_gdep) 
-      *old_gdep = NULL;
-
-    for (g = gdh->gdh_head; g; g = g->gde_next)
-    {
-      compare = (*gdh->gdh_oo_cmp)(g, gde);
-
-      if (compare == 0)
-      {
-	if (unique_keys)
-	{
-	  if (old_gdep)
-	    *old_gdep = g;
-	  gdh->gdh_errno = DICT_EEXISTS;
-	  ret = DICT_ERR;
-	}
-	else
-	{
-	  if (ordered)
-	  {
-	    BK_DLL_INSERT_BEFORE(gdh, gde, g);
-	    break;
-	  }
-	}
-      }
-      else if (compare > 0)
-      {
-	if (ordered)
-	{
-	  BK_DLL_INSERT_BEFORE(gdh, gde, g);
-	  break;
-	}
-      }
-    }
-    
-    if (ret == DICT_OK)
-    {
-      if (ordered && ret == DICT_OK)
-      {
-	BK_DLL_INSERT_AFTER(gdh, gde, gdh->gdh_tail);
-      }
-      else 
-      {
-	if (append)
-	  BK_DLL_INSERT_AFTER(gdh, gde, gdh->gdh_tail);
-	else
-	  BK_DLL_INSERT_BEFORE(gdh, gde, gdh->gdh_head);
-      }
-    }
-  }
-  
-  return(DICT_OK);
-}
 
 
 
---
+++ lib/b_dll.c
@@ -0,0 +1,102 @@
+#if !defined(lint) && !defined(__INSIGHT__)
+static const char libbk__rcsid[] = "$Id: b_dll.c,v 1.1 2004/05/04 14:40:39 brian Exp $";
+static const char libbk__copyright[] = "Copyright (c) 2003";
+static const char libbk__contact[] = "<projectbaka@baka.org>";
+#endif /* not lint */
+
+#include <libbk.h>
+#include "libbk_internal.h"
+
+/**
+ * @file
+ *
+ * This file implements the "light weight" doubly linked lists that are mostly
+ * implmented as inlines in libbk_inline.h Some of the functions are a bit
+ * long for inlining and cause problems with the now functional inline warnings
+ * of gcc 3.3.x
+ */
+
+/**
+ * Insert a dll element onto the list. In keeping with CLC insert
+ * prepends. Again flags is supporessed to keep this in line with the CLC
+ * APi.
+ *
+ *	@param handle The handle of dll.
+ *	@param obj The object to insert.
+ *	@return !<i>DICT_ERR</i> on failure.<br>
+ *	@return <i>DICT_OK</i> on success.
+ */
+int
+bk_dll_insert_internal(struct bk_generic_dll_handle *gdh, struct bk_generic_dll_element *gde, struct bk_generic_dll_element **old_gdep, int flags, int append)
+{
+  struct bk_generic_dll_element *g;
+  int ret = DICT_OK;
+  int compare;
+
+  if (!flags)
+  {
+    // General case where we want an unordered list and duplicates are fine.
+    if (append)
+      BK_DLL_INSERT_AFTER(gdh, gde, gdh->gdh_tail);
+    else
+      BK_DLL_INSERT_BEFORE(gdh, gde, gdh->gdh_head);
+  }
+  else
+  {
+    // This sections covers *both* ordering and unique keys, but at least one must be present.
+    int ordered = flags & DICT_ORDERED;
+    int unique_keys = flags & DICT_UNIQUE_KEYS;
+
+    if (old_gdep) 
+      *old_gdep = NULL;
+
+    for (g = gdh->gdh_head; g; g = g->gde_next)
+    {
+      compare = (*gdh->gdh_oo_cmp)(g, gde);
+
+      if (compare == 0)
+      {
+	if (unique_keys)
+	{
+	  if (old_gdep)
+	    *old_gdep = g;
+	  gdh->gdh_errno = DICT_EEXISTS;
+	  ret = DICT_ERR;
+	}
+	else
+	{
+	  if (ordered)
+	  {
+	    BK_DLL_INSERT_BEFORE(gdh, gde, g);
+	    break;
+	  }
+	}
+      }
+      else if (compare > 0)
+      {
+	if (ordered)
+	{
+	  BK_DLL_INSERT_BEFORE(gdh, gde, g);
+	  break;
+	}
+      }
+    }
+    
+    if (ret == DICT_OK)
+    {
+      if (ordered && ret == DICT_OK)
+      {
+	BK_DLL_INSERT_AFTER(gdh, gde, gdh->gdh_tail);
+      }
+      else 
+      {
+	if (append)
+	  BK_DLL_INSERT_AFTER(gdh, gde, gdh->gdh_tail);
+	else
+	  BK_DLL_INSERT_BEFORE(gdh, gde, gdh->gdh_head);
+      }
+    }
+  }
+  
+  return(DICT_OK);
+}
