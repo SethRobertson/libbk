564000be353e503105a2a6ac0c3bf9944390f01a
Seth Robertson <seth@counterstorm.com>
2005-03-16 18:53:26 +0000

    Add adjtime for sites with incorrect timestamps where we should not step the clock

---
+++ src/adjtime.c
@@ -0,0 +1,43 @@
+/*
+ * Dedicated to the public.
+ *
+ * Example usage: adjtime `/usr/sbin/ntpdate  -d timex.cs.columbia.edu 2>/dev/null | egrep ^offset | sed 's/\./ /' | awk '{print $2,$3}'`
+ */
+
+#include <libbk.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+  struct timeval forward;
+  struct timeval oldforward;
+
+  if (argc != 3)
+  {
+    fprintf(stderr, "Usage: %s <seconds offset> <ms offset>\n", argv[0]);
+    exit(1);
+  }
+
+  forward.tv_sec = atoi(argv[1]);
+  forward.tv_usec = atoi(argv[2]);
+
+  if (forward.tv_sec > 2000)
+  {
+    printf("Maximum adjustment is 2000 or so, resetting maximum\n");
+    forward.tv_sec = 2000;
+  }
+  if (forward.tv_sec < 2000)
+  {
+    printf("Minimum adjustment is -2000 or so, resetting minium\n");
+    forward.tv_sec = -2000;
+  }
+
+  printf("Adjusting time by %ld.%06ld seconds\n", forward.tv_sec, forward.tv_usec);
+  if (adjtime(&forward, &oldforward) < 0)
+  {
+    perror("adjtime() failure");
+    exit(2);
+  }
+
+  printf("Old adjustment was %ld.%06ld seconds\n", oldforward.tv_sec, oldforward.tv_usec);
+  exit(0);
+}
