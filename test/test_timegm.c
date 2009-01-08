b11e78f2e8e102c09a8b2c673ff14469cdc82bcf
Seth Robertson <srobertson@counterstorm.com>
2009-01-08 15:17:23 -0500

    Bug 5346: Vaguely helpful test program for bk_timegm

---
+++ test/test_timegm.c
@@ -0,0 +1,137 @@
+#if !defined(lint)
+static const char libbk__copyright[] = "Copyright © 2002-2008";
+static const char libbk__contact[] = "<projectbaka@baka.org>";
+#endif /* not lint */
+/*
+ * ++Copyright BAKA++
+ *
+ * Copyright © 2002-2008 The Authors. All rights reserved.
+ *
+ * This source code is licensed to you under the terms of the file
+ * LICENSE.TXT in this release for further details.
+ *
+ * Send e-mail to <projectbaka@baka.org> for further information.
+ *
+ * - -Copyright BAKA- -
+ */
+
+/**
+ * @file
+ *
+ * Example libbk user with main() prototype.
+ */
+#include <libbk.h>
+
+
+#define ERRORQUEUE_DEPTH 32			///< Default depth
+
+/**
+ * Information of international importance to everyone
+ * which cannot be passed around.
+ */
+struct global_structure
+{
+  bk_flags	gs_flags;
+} Global;
+
+
+
+/**
+ * Information about basic program runtime configuration
+ * which must be passed around.
+ */
+struct program_config
+{
+  int pc_format_flags;
+};
+
+
+
+/**
+ * Program entry point
+ *
+ *	@param argc Number of argv elements
+ *	@param argv Program name and arguments
+ *	@param envp Program environment
+ *	@return <i>0</i> Success
+ *	@return <br><i>254</i> Initialization failed
+ */
+int
+main(int argc, char **argv, char **envp)
+{
+  bk_s B = NULL;				/* Baka general structure */
+  BK_ENTRY_MAIN(B, __FUNCTION__, __FILE__, "SIMPLE");
+  char c;
+  int getopterr=0;
+  extern char *optarg;
+  extern int optind;
+  struct program_config Pconfig, *pconfig=NULL;
+  poptContext optCon=NULL;
+  struct poptOption optionsTable[] =
+  {
+    {"debug", 'd', POPT_ARG_NONE, NULL, 'd', "Turn on debugging", NULL },
+    POPT_AUTOHELP
+    POPT_TABLEEND
+  };
+
+  if (!(B=bk_general_init(argc, &argv, &envp, BK_ENV_GWD(B, "BK_ENV_CONF_APP", BK_APP_CONF), NULL, ERRORQUEUE_DEPTH, LOG_USER, 0)))
+  {
+    fprintf(stderr,"Could not perform basic initialization\n");
+    exit(254);
+  }
+  bk_fun_reentry(B);
+
+  pconfig = &Pconfig;
+  memset(pconfig,0,sizeof(*pconfig));
+
+  if (!(optCon = poptGetContext(NULL, argc, (const char **)argv, optionsTable, 0)))
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Could not initialize options processing\n");
+    bk_exit(B,254);
+  }
+
+  while ((c = poptGetNextOpt(optCon)) >= 0)
+  {
+    switch (c)
+    {
+    case 'd':
+      bk_error_config(B, BK_GENERAL_ERROR(B), 0, stderr, 0, BK_ERR_DEBUG, BK_ERROR_CONFIG_FH);	// Enable output of all error logs
+      bk_general_debug_config(B, stderr, BK_ERR_NONE, 0);					// Set up debugging, from config file
+      bk_debug_printf(B, "Debugging on\n");
+      break;
+    default:
+      getopterr++;
+      break;
+    }
+  }
+
+  if (c < -1 || getopterr)
+  {
+    if (c < -1)
+    {
+      fprintf(stderr, "%s\n", poptStrerror(c));
+    }
+    poptPrintUsage(optCon, stderr, 0);
+    bk_exit(B,254);
+  }
+
+  {
+    struct tm tm;
+    time_t ret, last = 0;
+
+    tm.tm_sec = 0;
+    tm.tm_min = 0;
+    tm.tm_hour = 0;
+    tm.tm_mday = 0;
+    tm.tm_year = 0;
+
+    for (tm.tm_year = 100; tm.tm_year < 20000; tm.tm_year+=400)
+    {
+      ret = bk_timegm(B, &tm, 0);
+      printf("%5d %lld %lld\n", tm.tm_year+1900, (long long)(ret-last), (long long)ret);
+      last = ret;
+    }
+  }
+
+  BK_RETURN(B,0);
+}
