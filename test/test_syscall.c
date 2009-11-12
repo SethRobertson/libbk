1ca17ed98da6bd2c16ba8f5d26fac4f98843bf78
James Tanis <jtt@tanis.org>
2009-11-12 17:41:19 -0500

    system call timer

---
+++ test/test_syscall.c
@@ -0,0 +1,374 @@
+#if !defined(lint)
+static const char libbk__copyright[] = "Copyright © 2001-2008";
+static const char libbk__contact[] = "<projectbaka@baka.org>";
+#endif /* not lint */
+/*
+ * ++Copyright BAKA++
+ *
+ * Copyright © 2001-2008 The Authors. All rights reserved.
+ *
+ * This source code is licensed to you under the terms of the file
+ * LICENSE.TXT in this release for further details.
+ *
+ * Send e-mail to <projectbaka@baka.org> for further information.
+ *
+ * - -Copyright BAKA- -
+ */
+
+/**
+ * @file
+ *
+ * XXX - customize here
+ * Example libbk user with main() prototype.
+ */
+#include <libbk.h>
+#include <libbk_i18n.h>
+
+
+#define STD_LOCALEDIR_KEY     "LOCALEDIR"	///< Key in bkconfig to find the locale translation files
+#define STD_LOCALEDIR_ENV     "BAKA_HOME"	///< Key in Environment to find base of locale directory
+#define STD_LOCALEDIR_DEF     "/usr/local/baka"	///< Default base of where locale directory might be found
+#define STD_LOCALEDIR_SUB     "locale"		///< Sub-component from install base where locale might be found
+#define ERRORQUEUE_DEPTH      32		///< Default error queue depth
+
+
+
+int get_time_diffs(bk_s B, double *user_diffp, double *system_diffp, struct rusage *ru_start, struct rusage *ru_end, bk_flags flags);
+
+
+/**
+ * Information of international importance to everyone
+ * which cannot be passed around.
+ */
+struct global_structure
+{
+} Global;
+
+
+
+/**
+ * Information about basic program runtime configuration
+ * which must be passed around.
+ */
+struct program_config
+{
+  int			pc_count;
+  bk_flags		pc_flags;		///< Flags are fun!
+#define PC_VERBOSE	0x001			///< Verbose output
+};
+
+
+
+static int proginit(bk_s B, struct program_config *pc, int argc, char **argv);
+static void progrun(bk_s B, struct program_config *pc);
+
+
+
+/**
+ * Program entry point
+ *
+ *	@param argc Number of argv elements
+ *	@param argv Program name and arguments
+ *	@param envp Program environment
+ *	@return <i>0</i> Success
+ *	@return <br><i>254</i> Initialization failed
+ */
+int
+main(int argc, char **argv, char **envp)
+{
+  bk_s B = NULL;				/* Baka general structure */
+  BK_ENTRY_MAIN(B, __FUNCTION__, __FILE__, "test_syscall");
+  int c;
+  int getopterr = 0;
+  int debug_level = 0;
+  char i18n_localepath[_POSIX_PATH_MAX];
+  char *i18n_locale;
+  struct program_config Pconfig, *pc = NULL;
+  poptContext optCon = NULL;
+  struct poptOption optionsTable[] =
+  {
+    {"debug", 'd', POPT_ARG_NONE, NULL, 'd', N_("Turn on debugging"), NULL },
+    {"verbose", 'v', POPT_ARG_NONE, NULL, 'v', N_("Turn on verbose message"), NULL },
+    {"count", 'c', POPT_ARG_INT, NULL, 'c', N_("Number of iterations"), NULL },
+    {"no-seatbelts", 0, POPT_ARG_NONE, NULL, 0x1000, N_("Sealtbelts off & speed up"), NULL },
+    {"seatbelts", 0, POPT_ARG_NONE, NULL, 0x1001, N_("Enable function tracing"), NULL },
+    {"profiling", 0, POPT_ARG_STRING, NULL, 0x1002, N_("Enable and write profiling data"), N_("filename") },
+
+    {"long-arg-only", 0, POPT_ARG_NONE, NULL, 1, N_("An example of a long argument without a shortcut"), NULL },
+    {NULL, 's', POPT_ARG_NONE, NULL, 2, N_("An example of a short argument without a longcut"), NULL },
+    POPT_AUTOHELP
+    POPT_TABLEEND
+  };
+
+  if (!(B=bk_general_init(argc, &argv, &envp, BK_ENV_GWD(NULL, "BK_ENV_CONF_APP", BK_APP_CONF), NULL, ERRORQUEUE_DEPTH, LOG_LOCAL0, 0)))
+  {
+    fprintf(stderr,"Could not perform basic initialization\n");
+    exit(254);
+  }
+  bk_fun_reentry(B);
+
+  // Enable error output
+  bk_error_config(B, BK_GENERAL_ERROR(B), ERRORQUEUE_DEPTH, stderr, BK_ERR_ERR,
+		  BK_ERR_ERR, BK_ERROR_CONFIG_FH |
+		  BK_ERROR_CONFIG_SYSLOGTHRESHOLD | BK_ERROR_CONFIG_HILO_PIVOT);
+
+  // i18n stuff
+  setlocale(LC_ALL, "");
+  if (!(i18n_locale = BK_GWD(B, STD_LOCALEDIR_KEY, NULL)))
+  {
+    i18n_locale = i18n_localepath;
+    snprintf(i18n_localepath, sizeof(i18n_localepath), "%s/%s", BK_ENV_GWD(B, STD_LOCALEDIR_ENV,STD_LOCALEDIR_DEF), STD_LOCALEDIR_SUB);
+  }
+  bindtextdomain(BK_GENERAL_PROGRAM(B), i18n_locale);
+  textdomain(BK_GENERAL_PROGRAM(B));
+  for (c = 0; optionsTable[c].longName || optionsTable[c].shortName; c++)
+  {
+    if (optionsTable[c].descrip) (*((char **)&(optionsTable[c].descrip)))=_(optionsTable[c].descrip);
+    if (optionsTable[c].argDescrip) (*((char **)&(optionsTable[c].argDescrip)))=_(optionsTable[c].argDescrip);
+  }
+
+  pc = &Pconfig;
+  memset(pc, 0, sizeof(*pc));
+
+  pc->pc_count = 100000;
+
+  if (!(optCon = poptGetContext(NULL, argc, (const char **)argv, optionsTable, 0)))
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Could not initialize options processing\n");
+    bk_exit(B, 254);
+  }
+
+  poptSetOtherOptionHelp(optCon, _("[NON-FLAG ARGUMENTS]"));
+
+  while ((c = poptGetNextOpt(optCon)) >= 0)
+  {
+    switch (c)
+    {
+    case 'd':					// debug
+      if (!debug_level)
+      {
+	// Set up debugging, from config file
+	bk_general_debug_config(B, stderr, BK_ERR_NONE, 0);
+	bk_debug_printf(B, "Debugging on\n");
+	debug_level++;
+      }
+      else if (debug_level == 1)
+      {
+	/*
+	 * Enable output of error and higher error logs (this can be
+	 * annoying so require -dd)
+	 */
+	bk_error_config(B, BK_GENERAL_ERROR(B), 0, stderr, BK_ERR_NONE, BK_ERR_ERR, BK_ERROR_CONFIG_FH | BK_ERROR_CONFIG_HILO_PIVOT | BK_ERROR_CONFIG_SYSLOGTHRESHOLD);
+	bk_debug_printf(B, "Extra debugging on\n");
+	debug_level++;
+      }
+      else if (debug_level == 2)
+      {
+	/*
+	 * Enable output of all levels of bk_error logs (this can be
+	 * very annoying so require -ddd)
+	 */
+	bk_error_config(B, BK_GENERAL_ERROR(B), 0, stderr, BK_ERR_NONE, BK_ERR_DEBUG, BK_ERROR_CONFIG_FH | BK_ERROR_CONFIG_HILO_PIVOT | BK_ERROR_CONFIG_SYSLOGTHRESHOLD);
+	bk_debug_printf(B, "Super-extra debugging on\n");
+	debug_level++;
+      }
+      break;
+    case 'v':					// verbose
+      BK_FLAG_SET(pc->pc_flags, PC_VERBOSE);
+      bk_error_config(B, BK_GENERAL_ERROR(B), ERRORQUEUE_DEPTH, stderr, BK_ERR_NONE, BK_ERR_ERR, 0);
+      break;
+    case 'c':					// verbose
+      pc->pc_count = atoi(poptGetOptArg(optCon));
+      break;
+    case 0x1000:				// no-seatbelts
+      BK_FLAG_CLEAR(BK_GENERAL_FLAGS(B), BK_BGFLAGS_FUNON);
+      break;
+    case 0x1001:				// seatbelts
+      BK_FLAG_SET(BK_GENERAL_FLAGS(B), BK_BGFLAGS_FUNON);
+      break;
+    case 0x1002:				// profiling
+      bk_general_funstat_init(B, (char *)poptGetOptArg(optCon), 0);
+      break;
+    default:
+      getopterr++;
+      break;
+
+    case 1:					// long-arg-only
+      printf("You specificed the long-arg-only option\n");
+      break;
+    case 2:					// s
+      printf("You specificed the short only option\n");
+      break;
+    }
+  }
+
+  /*
+   * Reprocess so that argc and argv contain the remaining command
+   * line arguments (note argv[0] is an argument, not the program
+   * name).  argc remains the number of elements in the argv array.
+   */
+  argv = (char **)poptGetArgs(optCon);
+  argc = 0;
+  if (argv)
+    for (; argv[argc]; argc++)
+      ; // Void
+
+  if (c < -1 || getopterr)
+  {
+    if (c < -1)
+    {
+      fprintf(stderr, "%s\n", poptStrerror(c));
+    }
+    poptPrintUsage(optCon, stderr, 0);
+    bk_exit(B, 254);
+  }
+
+  if (proginit(B, pc, argc, argv) < 0)
+  {
+    bk_die(B, 254, stderr, _("Could not perform program initialization\n"), BK_FLAG_ISSET(pc->pc_flags, PC_VERBOSE)?BK_WARNDIE_WANTDETAILS:0);
+  }
+
+  progrun(B, pc);
+
+  poptFreeContext(optCon);
+  bk_exit(B, 0);
+  return(255);					// Stupid INSIGHT stuff.
+}
+
+
+
+/**
+ * General program initialization
+ *
+ *	@param B BAKA Thread/Global configuration
+ *	@param pc Program configuration
+ *	@return <i>0</i> Success
+ *	@return <br><i>-1</i> Total terminal failure
+ */
+static int proginit(bk_s B, struct program_config *pc, int argc, char **argv)
+{
+  BK_ENTRY(B, __FUNCTION__,__FILE__,"test_syscall");
+
+  if (!pc)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Invalid argument\n");
+    BK_RETURN(B, -1);
+  }
+
+
+  BK_RETURN(B, 0);
+}
+
+
+
+/**
+ * Normal processing of program
+ *
+ *	@param B BAKA Thread/Global configuration
+ *	@param pc Program configuration
+ *	@return <i>0</i> Success--program may terminate normally
+ *	@return <br><i>-1</i> Total terminal failure
+ */
+static void progrun(bk_s B, struct program_config *pc)
+{
+  BK_ENTRY(B, __FUNCTION__,__FILE__,"test_syscall");
+  struct rusage start;
+  struct rusage after_syscall;
+  struct rusage after_getpid;
+  int cnt;
+  struct timeval tv;
+  struct timezone tz;
+  double user_diff, system_diff;
+
+  if (!pc)
+  {
+    bk_error_printf(B, BK_ERR_ERR, "Invalid argument\n");
+    BK_VRETURN(B);
+  }
+
+  if (getrusage(RUSAGE_SELF, &start) < 0)
+  {
+    perror("Start rusage failed");
+    exit(1);
+  }
+
+  for (cnt = 0; cnt < pc->pc_count; cnt++)
+  {
+    // Insert your system call here for testing.
+    if (gettimeofday(&tv, &tz) < 0)
+    {
+      perror("gettimeofday failed");
+      exit(1);
+    }
+  }
+
+  if (getrusage(RUSAGE_SELF, &after_syscall) < 0)
+  {
+    perror("After syscall rusage failed");
+    exit(1);
+  }
+
+  for (cnt = 0; cnt < pc->pc_count; cnt++)
+  {
+    // Always do a getpid test for comparison.
+    if (getpid() < 0)
+    {
+      perror("getpid failed");
+      exit(1);
+    }
+  }
+
+  if (getrusage(RUSAGE_SELF, &after_getpid) < 0)
+  {
+    perror("After getpid rusage failed");
+    exit(1);
+  }
+
+  printf("Number of iterations: %d\n", pc->pc_count);
+  printf("%20s | %10s | %10s\n", "syscall", "user", "system");
+  if (get_time_diffs(B, &user_diff, &system_diff, &after_syscall, &start, 0) < 0)
+  {
+    fprintf(stderr, "Could not obtain gettimeofday times\n");
+    exit(1);
+  }
+  printf("%20s | %10.3lf | %10.3lf\n", "gettimeofday", user_diff, system_diff);
+
+  if (get_time_diffs(B, &user_diff, &system_diff, &after_getpid, &after_syscall, 0) < 0)
+  {
+    fprintf(stderr, "Could not obtain getpid times\n");
+    exit(1);
+  }
+  printf("%20s | %10.3lf | %10.3lf\n", "getpid", user_diff, system_diff);
+
+  BK_VRETURN(B);
+}
+
+
+/**
+ *
+ *
+ *	@param B BAKA thread/global state.
+ *	@param flags Flags for future use.
+ *	@return <i>-1</i> on failure.<br>
+ *	@return <i>0</i> on success.
+ */
+int
+get_time_diffs(bk_s B, double *user_diffp, double *system_diffp, struct rusage *ru_start, struct rusage *ru_end, bk_flags flags)
+{
+  BK_ENTRY(B, __FUNCTION__,__FILE__,"test_syscall");
+  struct timeval diff;
+
+  if (!user_diffp || !system_diffp || !ru_start || !ru_end)
+  {
+    bk_error_printf(B, BK_ERR_ERR,"Illegal arguments\n");
+    BK_RETURN(B, -1);
+  }
+
+  BK_TV_SUB((&diff), (&(ru_start->ru_utime)), (&(ru_end->ru_utime)));
+  *user_diffp = (double)(diff.tv_sec) + (double)(diff.tv_usec / 1000000.0);
+
+  BK_TV_SUB((&diff), (&(ru_start->ru_stime)), (&(ru_end->ru_stime)));
+  *system_diffp = (double)(diff.tv_sec) + (double)(diff.tv_usec / 1000000.0);
+
+  BK_RETURN(B, 0);
+}
